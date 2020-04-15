1be935aed00cd23cd105656192c8ce9c7fdbee3c
amulsikchi <amul.sikchi@gmail.com>
2020-04-15 12:33:31 +0530

    Initial Folder Structure as per CoC
    Initial Folder Structure as per CoC

--- README.md
+++
@@ -1,2 +0,0 @@
-# c19-store-fulfillment
-Store Fulfillment in 30 days
--- src/main/java/com/ibm/sterling/App.java
+++
@@ -1,7 +0,0 @@
-package com.ibm.sterling
-
-public class App {
-   public static void main(String[] args) {
-       System.out.println("First App");
-   }
-}
